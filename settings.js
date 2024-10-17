e67ba416a4aff4019f97d2897680f0f1569f6db8
Issei <74949779+VaseemStarkJr3000@users.noreply.github.com>
2024-10-17 10:03:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'My Sticker Pack',
-  author: 'My Bot',
-  botName: "Knight Bot",
+  packname: 'issei bot',
+  author: 'by vaseem',
+  botName: "Rias",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '916305309081', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
